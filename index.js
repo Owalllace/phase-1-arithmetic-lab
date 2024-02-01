afbc612dd83eee7419f65774df55dfbbd1b07295
OliviaWallace <oliviawallacepdx@gmail.com>
2024-01-31 19:37:32 -0800

    assignment complete.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = 31 * 2;
 
-// Write your code here
+Math.floor()
+let random = 1;
+
+let num3 = 9;
+let num4 = 5;
+let mod = 9 % 5;
+
+Math.max(19,20);
+let max = 20;
